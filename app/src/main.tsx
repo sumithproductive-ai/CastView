cacc622fbf099ae54e843538b641ed2adf855e4b
sumithproductive-ai <sumithproductive@gmail.com>
2026-05-29 11:15:26 -0500

    Update main.tsx

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -2,7 +2,12 @@ import React from 'react';
 
   import { createRoot } from "react-dom/client";
   import App from "./app/App.tsx";
+  import { AuthProvider } from './app/context/AuthContext';
   import "./styles/index.css";
 
-  createRoot(document.getElementById("root")!).render(<App />);
+  createRoot(document.getElementById("root")!).render(
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  );
   
